ceccc309097e42f3fa11b6f65fdde219a79e7942
the-menna-sherif <mennaseducation@gmail.com>
2025-12-12 14:56:39 +0200

    Updated sentiment.py

--- text-classification/sentiment-analyzer.py
+++ text-classification/sentiment-analyzer.py
@@ -3,16 +3,18 @@ from transformers import pipeline
 import gradio as gr
 import torch
 
-model_path = r"C:\Users\msherif\PycharmProjects\GenAIProjects\venv\Models\models--distilbert--distilbert-base-uncased-finetuned-sst-2-english"
-
 # Load a pre-trained sentiment analysis model
 # cannot have the full path if pushing the code to Hugging Face Hub
-# pipe = pipeline("text-classification", 
-               # model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 analyzer = pipeline("text-classification", 
-                model=model_path)
+               model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+# model_path = "C:/Users/msherif/PycharmProjects/GenAIProjects/text-classification/sentiment-model"
+# pipe = pipeline("text-classification", 
+#                 model=model_path)
 
 print(analyzer(["This is a good product",
-                 "This was a very expensive product."
-                 ]
-                 ))
+                 "This was a very expensive product."])
+    )
+
+def sentiment_analysis(review):
+    sentiment = analyzer(review)
+    return sentiment
