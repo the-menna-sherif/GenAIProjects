1fe3e41f1f9a5bf32089787effa673ba9d6a374b
the-menna-sherif <mennaseducation@gmail.com>
2025-12-12 16:08:11 +0200

    Friday work

--- text-classification/sentiment-analyzer.py
+++ text-classification/sentiment-analyzer.py
@@ -2,6 +2,7 @@
 from transformers import pipeline
 import gradio as gr
 import torch
+import pandas as pd
 
 # Load a pre-trained sentiment analysis model
 # cannot have the full path if pushing the code to Hugging Face Hub
@@ -26,4 +27,22 @@ demo = gr.Interface(
     title="@GenAI Project 3: Sentiment Analyzer",
     description="Analyze the sentiment based on provided comments."
 )
-demo.launch()
+# demo.launch()
+
+file_path = "C:/Users/msherif/PycharmProjects/GenAIProjects/text-classification/reviews.xlsx"
+
+def read_reviews_analyze_sentiment(file_path):
+    # load excel file into dataframe
+    df = pd.read_excel(file_path)
+
+    # check if review column exists in DF
+    if 'Review' not in df.columns:
+        raise ValueError("The provided file does not contain a 'review' column.")
+    
+    # apply sentiment analysis on each review
+    df['Sentiment'] = df['Review'].apply(sentiment_analyzer)
+    return df
+
+file_path = r"text-classification\Files\sample_reviews.xlsx"
+result = read_reviews_analyze_sentiment(file_path)
+print(result)
