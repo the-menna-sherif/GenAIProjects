5956da7a5ee46f0b1adc54367fa20248a2f6469c
the-menna-sherif <mennaseducation@gmail.com>
2025-12-15 18:31:53 +0200

    Added dynamic Gradio GUI code.

--- text-classification/sentiment-analyzer.py
+++ text-classification/sentiment-analyzer.py
@@ -20,20 +20,14 @@ def sentiment_analyzer(review):
     sentiment = analyzer(review)
     return sentiment[0]['label']
 
-demo = gr.Interface(
-    fn=sentiment_analyzer,
-    inputs=[gr.Textbox(label="Enter your review here", lines=4)],
-    outputs=[gr.Textbox(label="Sentiment", lines=1)],
-    title="@GenAI Project 3: Sentiment Analyzer",
-    description="Analyze the sentiment based on provided comments."
-)
-# demo.launch()
 
 # file_path = r"C:/Users/msherif/PycharmProjects/GenAIProjects/text-classification/reviews.xlsx"
 
-def read_reviews_analyze_sentiment(file_path):
+# function to read reviews from excel file and analyze sentiment
+# modified to accept file object from Gradio File input (no code change- just file_path to file_object)
+def read_reviews_analyze_sentiment(file_object):
     # load excel file into dataframe
-    df = pd.read_excel(file_path)
+    df = pd.read_excel(file_object)
 
     # check if review column exists in DF
     if 'Review' not in df.columns:
@@ -43,8 +37,29 @@ def read_reviews_analyze_sentiment(file_path):
     df['Sentiment'] = df['Review'].apply(sentiment_analyzer)
     return df
 
+# Prototype Gradio interface, input is a textbox, output is a textbox (based on single review analysis)
+demo = gr.Interface(
+    fn=sentiment_analyzer,
+    inputs=[gr.Textbox(label="Enter your review here", lines=4)],
+    outputs=[gr.Textbox(label="Sentiment", lines=1)],
+    title="@GenAI Project 3: Sentiment Analyzer",
+    description="Analyze the sentiment based on provided comments."
+)
+
+# Dynamic gradio interface, input is a review excel, output is a pandas dataframe 
+# (based on multiple reviews analyses)
+demo_dynamic = gr.Interface(
+    fn=read_reviews_analyze_sentiment,
+    inputs=[gr.File(file_types=[".xlsx"],label="Upload your review file")],
+    outputs=[gr.DataFrame(label="Sentiments")],
+    title="@GenAI Project 3: Sentiment Analyzer",
+    description="Analyze the sentiment based on provided review file uploaded."
+)
+# demo.launch()
+demo_dynamic.launch()
+
 file_path = r"C:\Users\msherif\PycharmProjects\GenAIProjects\text-classification\Files\sample_reviews.xlsx"
 # the relative path would look like this:
 # file_path = r"./Files/sample_reviews.xlsx"
-result = read_reviews_analyze_sentiment(file_path)
-print(result)
+# result = read_reviews_analyze_sentiment(file_path)
+# print(result)
