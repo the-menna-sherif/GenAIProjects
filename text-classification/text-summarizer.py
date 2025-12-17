8859f51064614218dd545a9dd164d1d91964da2e
Menna <mennaseducation@gmail.com>
2025-12-17 17:19:44 +0200

    Refactor text summarizer to load model locally
    Added model & tokenizer code explicitly to resolve errors.

--- text-classification/text-summarizer.py
+++ text-classification/text-summarizer.py
@@ -1,21 +1,30 @@
 import torch
 import gradio as gr
 # Using pipeline as a high-level helper
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+import os
 
-text_summary = pipeline(task="summarization", model="sshleifer/distilbart-cnn-12-6",
-                torch_dtype=torch.bfloat16) # This compresses the model, without degrading performance
+model = "sshleifer/distilbart-cnn-12-6"
 
-# text = "A fairy (also called fay, fae, fae folk, fey, fair folk, or faerie) is a type of mythical being or legendary creature, generally described as anthropomorphic, found in the folklore of multiple European cultures (including Celtic, Slavic, Germanic, and French folklore), a form of spirit, often with magical, metaphysical, supernatural, or preternatural qualities. Myths and stories about fairies do not have a single origin but are rather a collection of folk beliefs from disparate sources. Various folk theories about the origins of fairies include casting them as either demoted angels or demons in a Christian tradition, as deities in pagan belief systems, as spirits of the dead, as prehistoric precursors to humans, or as spirits of nature."
-# print(text_summary(text))
+# Load tokenizer & model locally
+tokenizer = AutoTokenizer.from_pretrained(
+    model,
+    local_files_only=True
+)
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    model,
+    torch_dtype=torch.float16,
+    local_files_only=True
+)
+text_summary = pipeline(task="summarization", model=model,
+                dtype =torch.float16, tokenizer=tokenizer) 
 
 def summarize(text):
     output = text_summary(text)
     return output[0]['summary_text']
 
-gr.close_all()
-# demo = gr.Interface(fn=summarize, inputs="text", outputs="text")
-demo = gr.Interface(summarize,
+# gr.close_all()
+demo = gr.Interface(fn=summarize,
                     inputs=[gr.Textbox(label="Enter text to summarize",lines=6)],
                     outputs=[gr.Textbox(label="Summarized text",lines=4)],
                     title="GenAI Project 1: Text Summarizer",
