770cbb7a2f7772b7ec80cade3f48b2c543ad7184
the-menna-sherif <mennaseducation@gmail.com>
2026-02-02 14:47:31 +0200

    Updated to use arxiv instead of ArxivRetriever (and corr error/ progress logged).

--- ArXivResearchAssistant/retrieval.py
+++ ArXivResearchAssistant/retrieval.py
@@ -1,9 +1,25 @@
-from langchain_community.retrievers import ArxivRetriever
+from langchain_core.documents import Document
+import arxiv
+from pprint import pp
 
-retriever = ArxivRetriever(
-    load_max_docs=2,
-    get_full_documents=True,
-)
+def paper_with_ID(query: str, k: int):
+    search = arxiv.Search(query=query, max_results=k, sort_by=arxiv.SortCriterion.Relevance)
+    docs = []
+    for d in search.results():
+        docs.append(
+            Document(
+                page_content=d.summary,
+                metadata={
+                    "id": d.entry_id,
+                    "title": d.title,
+                    "authors": [a.name for a in d.authors],
+                    "published": str(d.published),
+                    "pdf_url": d.pdf_url,
+                }
+            )
+        )
+    return docs
 
-docs = retriever.invoke("Attention is all you need?")
-print(docs[0].metadata)
+query = "Attention Is All You Need"
+docs = paper_with_ID(query, k=2)
+pp([doc.metadata["title"] for doc in docs])
